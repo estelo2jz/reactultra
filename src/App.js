5fc46e7b0b8a36198951c6ee7784e46a847581b6
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-10-18 08:36:56 -0500

    added navlink, navmenu, navicon, navlinks styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import GlobalStyle from "./globalStyles";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Navbar } from "./components";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <GlobalStyle />
       <Navbar />
-    </div>
+    </Router>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import { Nav, NavbarContainer } from './Navbar.elements';
+import React, {useState} from 'react';
+import { FaTimes, FaBars } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+import { 
+  Nav, 
+  NavbarContainer, 
+  NavLogo, 
+  NavIcon, 
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks
+} from './Navbar.elements';
 
 function Navbar() {
+
+  const [click, setClick] = useState(false); 
+  // is gonna go false to true , false to true
+  const handleClick = () => setClick(!click)
+
   return (
     <>
-      <Nav>
-        <NavbarContainer>
-          <NavLogo to="/">
-            <NavIcon />
-            ULTRA
-          </NavLogo>
-        </NavbarContainer>
-      </Nav>
+      <IconContext.Provider value={{ color: '#fff'}}>
+        <Nav>
+          <NavbarContainer>
+            <NavLogo to="/">
+              <NavIcon />
+              ULTRA
+            </NavLogo>
+            <MobileIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+            <NavMenu onClick={handleClick}>
+              <NavItem>
+                <NavLinks to="/">
+                  Home
+                </NavLinks>
+              </NavItem>
+            </NavMenu>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
     </>
   )
 }
